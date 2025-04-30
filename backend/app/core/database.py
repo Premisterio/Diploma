a1433c3485982b1ca0e2714fee6702bddfbbe19f
Volodymyr <premisterio007@gmail.com>
2025-04-30 03:48:27 +0300

    Updated database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,18 +2,23 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from typing import Generator
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-from sqlalchemy.orm import Session
-from typing import Generator
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
